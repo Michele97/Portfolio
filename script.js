4da0d65699d8980340a60fa65fac11a5930aa638
B4ffo <pinto.michele1997@gmail.com>
2023-05-29 19:38:12 +0200

    ADD  Portfolio

---
+++ script.js
@@ -0,0 +1,51 @@
+ function ShowN(){
+        document.getElementById("descN").classList.remove("hide");
+        document.getElementById("descN").classList.add("show");
+        document.getElementById("boxMain").classList.add("opacity");
+        document.getElementById("boxMain").classList.add("blur");
+        document.getElementById("Vue").disable=true;
+        document.getElementById("Next").disable=true;
+        document.getElementById("Laravel").disable=true;
+ }
+ function ShowV(){
+        document.getElementById("descV").classList.remove("hide");
+        document.getElementById("descV").classList.add("show");
+        document.getElementById("boxMain").classList.add("opacity");
+        document.getElementById("boxMain").classList.add("blur");
+        document.getElementById("Vue").disable=true;
+        document.getElementById("Next").disable=true;
+        document.getElementById("Laravel").disable=true;
+ }
+ function ShowL(){
+        document.getElementById("descL").classList.remove("hide");
+        document.getElementById("descL").classList.add("show");
+        document.getElementById("boxMain").classList.add("opacity");
+        document.getElementById("boxMain").classList.add("blur");
+        document.getElementById("Vue").disable=true;
+        document.getElementById("Next").disable=true;
+        document.getElementById("Laravel").disable=true;
+        
+ }
+
+ window.onclick = function(event) {
+    
+    if (!event.target.matches('.imgbox')) {
+
+      var dropdowns = document.getElementsByClassName("desc");
+      var i;
+
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+          openDropdown.classList.add('hide');
+          document.getElementById("boxMain").classList.remove("opacity");
+          document.getElementById("boxMain").classList.remove("blur");
+
+        }
+      }
+    }
+  }
+
+
+
