b6adf6beffaa44fb1873280c0ce7af150451aadb
B4ffo <pinto.michele1997@gmail.com>
2023-07-14 12:05:59 +0200

    UPdate

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
  function ShowN(){
-        document.getElementById("descN").classList.remove("hide");
+        document.getElementById("descN").classList.remove('hide');
         document.getElementById("descN").classList.add("show");
         document.getElementById("boxMain").classList.add("opacity");
         document.getElementById("boxMain").classList.add("blur");
         document.getElementById("Vue").disable=true;
         document.getElementById("Next").disable=true;
         document.getElementById("Laravel").disable=true;
+
  }
+
  function ShowV(){
         document.getElementById("descV").classList.remove("hide");
         document.getElementById("descV").classList.add("show");
@@ -15,7 +17,9 @@
         document.getElementById("Vue").disable=true;
         document.getElementById("Next").disable=true;
         document.getElementById("Laravel").disable=true;
+       
  }
+
  function ShowL(){
         document.getElementById("descL").classList.remove("hide");
         document.getElementById("descL").classList.add("show");
@@ -24,17 +28,19 @@
         document.getElementById("Vue").disable=true;
         document.getElementById("Next").disable=true;
         document.getElementById("Laravel").disable=true;
+      
         
  }
 
  window.onclick = function(event) {
     
-    if (!(event.target.matches('.desc'))  ) {
+    if ((event.target.matches('.close'))  ) {
 
       var dropdowns = document.getElementsByClassName("desc");
       var i;
 
       for (i = 0; i < dropdowns.length; i++) {
+        
         var openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
